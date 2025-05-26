e7f975891f5ed0906aff0cbc6b60b52de3df0f58
Hunter <eugyneehunter@gmail.com>
2025-05-26 15:34:50 +0300

    Update server configuration to use PORT 5000 and improve dotenv integration

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Get request received');
-}
-);
+});
+
+// console.log(`Port: ${process.env.PORT}`);
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
