e50eacc8ed7f7958aa4692e7c5762957288286df
Hunter <eugyneehunter@gmail.com>
2025-05-26 14:43:05 +0300

    Initialize backend with Express server and package.json setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Get request received');
+}
+);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
