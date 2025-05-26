4f2c153f532fc9b9ec22e420220d880eecf80ff8
Hunter <eugyneehunter@gmail.com>
2025-05-26 22:14:27 +0300

    feat(auth): integrate Clerk for authentication with sign-in and sign-up flows
    - Added Clerk Expo SDK for authentication management.
    - Implemented sign-in and sign-up screens with email and password functionality.
    - Created verification step for email during sign-up.
    - Added layout components to manage authenticated and unauthenticated routes.
    - Introduced SafeScreen component for consistent padding with safe area insets.
    - Implemented sign-out functionality with a dedicated button.
    - Updated package.json to include necessary dependencies.
    - Created .env file for environment variables.

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,16 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import SafeScreen from '@/components/SafeScreen'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
+  )
+
+
 }
