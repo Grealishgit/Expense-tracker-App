c80b9ba78bd860d725d74d9b22ab414431d5557e
Eugene <eugyneehunter@gmail.com>
2025-12-29 23:12:12 +0300

    feat: add Kcb, Loop, and Mpesa transaction components with basic structure

---
+++ mobile/components/loopTransactions.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoopTransactions = () => {
+    return (
+        <View>
+            <Text>loopTransactions</Text>
+        </View>
+    )
+}
+
+export default LoopTransactions
+
+const styles = StyleSheet.create({})
