2e6defbd599de3fb8ad5080a04e9283943eb38b0
Eugene <eugyneehunter@gmail.com>
2025-12-30 20:11:39 +0300

    Implement feature X to enhance user experience and fix bug Y in module Z

---
+++ mobile/lib/data.js
@@ -0,0 +1,156 @@
+// Transactio formats for DB
+export const kcbTransactions=[
+    {
+        "amount": 40,
+        "date": "28/12/2025",
+        "id": "TLSKD2A4JZ",
+        "party": "FAADI INVESTMENT (7930569)",
+        "rawDate": "2025-12-28T16:10:00.000Z",
+        "reference": "TLSKD2A4JZ",
+        "time": "07:10 PM",
+        "title": "Sent to KCB Account",
+        "type": "expense"
+    },
+    {
+        "amount": 60,
+        "date": "27/12/2025",
+        "id": "TLRKD27GYP",
+        "party": "FAADI INVESTMENT (7930569)",
+        "rawDate": "2025-12-27T16:21:00.000Z",
+        "reference": "TLRKD27GYP",
+        "time": "07:21 PM",
+        "title": "Sent to KCB Account",
+        "type": "expense"
+    },
+    {
+        "amount": 60,
+        "date": "26/12/2025",
+        "id": "TLQKD24RZY",
+        "party": "FAADI INVESTMENT (7930569)",
+        "rawDate": "2025-12-26T16:14:00.000Z",
+        "reference": "TLQKD24RZY",
+        "time": "07:14 PM",
+        "title": "Sent to KCB Account",
+        "type": "expense"
+    },
+    {
+        "amount": 80,
+        "id": "TJDKD7CTAU",
+        "party": "FAADI INVESTMENT (7930569)",
+        "rawDate": "2025-10-13T16: 47: 00.000Z",
+        "reference": "TJDKD7CTAU",
+        "time": "07:47 PM",
+        "title": "Sent to KCB Account",
+        "type": "expense"
+    }
+]
+
+
+
+const loopTransaction=[
+    {
+        "amount": 30,
+        "date": "12/11/2025",
+        "fee": 0,
+        "id": "NHL7CZ57JKF4",
+        "loopRef": "NHL7CZ57JKF4",
+        "mpesaRef": "TKCKD9Z6VY",
+        "party": "0742636835",
+        "rawDate": "2025-11-12T10:43:27.000Z",
+        "time": "1:43 PM",
+        "title": "Received via LOOP",
+        "type": "income"
+    },
+    {
+        "amount": 100,
+        "date": "12/11/2025",
+        "fee": 0,
+        "id": "NHL7CZ57JZER",
+        "loopRef": "NHL7CZ57JZER",
+        "mpesaRef": "TKCKD9Z2OV",
+        "party": "0742636835",
+        "rawDate": "2025-11-12T10: 41: 22.000Z",
+        "time": "1:41 PM",
+        "title": "Received via LOOP",
+        "type": "income"
+    },
+    {
+        "amount": 100,
+        "date": "12/11/2025",
+        "fee": 0,
+        "id": "NHL7CZ575XSX",
+        "loopRef": "NHL7CZ575XSX",
+        "mpesaRef": "TKCKD9Z2LL",
+        "party": "0742636835",
+        "rawDate": "2025-11-12T10: 35: 53.000Z",
+        "time": "1:35 PM",
+        "title": "Received via LOOP",
+        "type": "income"
+    },
+]
+
+
+export const mpesaTransactions=[
+    {
+        "amount": 45,
+        "date": "",
+        "id": "TI956N8TVX",
+        "newBalance": 26.38,
+        "party": "FAADI INVESTMENTS",
+        "rawDate": 2025-12-30T15: 27: 06.940Z,
+        "time": "",
+        "title": "Outgoing Payment",
+        "transactionCost": 0,
+        "type": "expense"
+    },
+    {
+        "amount": 60,
+        "date": "",
+        "id": "TI986M5S0M",
+        "newBalance": 21.38,
+        "party": "everlyne  kiima",
+        "rawDate": 2025-12-30T15: 27: 06.940Z,
+        "time": "",
+        "title": "Outgoing Payment",
+        "transactionCost": 0,
+        "type": "expense"
+    },
+    {
+        "amount": 20,
+        "date": "",
+        "id": "TI974DPFM3",
+        "newBalance": 81.38,
+        "party": "Cornelius Kisoo",
+        "rawDate": 2025-12-30T15: 27: 06.940Z,
+        "time": "",
+        "title": "Outgoing Payment",
+        "transactionCost": 0,
+        "type": "expense"
+    },
+    {
+        "amount": 600,
+        "date": "",
+        "id": "TI88252JJ6",
+        "newBalance": 600,
+        "party": "ERICK  SHIKANGA",
+        "rawDate": 2025-12-30T15: 27: 06.940Z,
+        "time": "",
+        "title": "Incoming Payment",
+        "transactionCost": 0,
+        "type": "income"
+    },
+    {
+        "amount": 500,
+        "date": "",
+        "id": "TIB2FF2PTG",
+        "newBalance": 500,
+        "party": "Kelvin  Muimi",
+        "rawDate": 2025-12-30T15: 27: 06.939Z,
+        "time": "",
+        "title": "Incoming Payment",
+        "transactionCost": 0,
+        "type": "income"
+    },
+    
+    
+]
